0f5da895f4b8f217d5a7f2effe9be1a34881bffa
Problem <pboyd04@gmail.com>
2014-08-26 15:46:35 +0000

    Fix typo

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $page->body .= '
     <h1>Welcome to the Burning Flipside Profile System</h1>
     <p>This system allows you to login to the new and improved Burning Flipside website systems.</p>
     <p>This system will contain all your private data seperately so as to help prevent unwanted display of your data on www.burningflipside.com.
-       Additionally, completing your profile on this site will enable you complete ticket requests and sign up for volunteer shifts even faster than before.</p>
+       Additionally, completing your profile on this site will enable you to complete ticket requests and sign up for volunteer shifts even faster than before.</p>
     <h1>Need to register for the first time?</h1>
     <p>You can sign up for an account <a href="register.php">here</a>.
     <h1>Forgot your username or password?</h1>
