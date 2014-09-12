d0d179f94b610428e75fa4065ef492014aa54d68
Problem <pboyd04@gmail.com>
2014-09-12 22:17:33 +0000

    Add a page to be shown when the browser is bad

---
+++ badbrowser.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<h2>Incompatible Browser Warning</h2>
+<p>The Web-Browser you use is out-of-date or not supported. To see the content on this Page correctly you have to update youre Browser or install a new one.</p> 
+<p>Please Update your browser or get a new one instead.</p>
+<ul>
+    <li><a href="http://www.mozilla.org/en-US/firefox/all.html">Download Firefox</a></li>
+    <li><a href="https://www.google.com/chrome">Download Chrome</a></li>
+    <li><a href="http://www.apple.com/safari/download/">Download Safari</a></li>
+    <li><a href="http://www.opera.com/download/">Download Opera</a></li>
+    <li><a href="http://windows.microsoft.com/en-US/internet-explorer/downloads/ie/">Download Internet Explorer</a></li>
+</ul>
