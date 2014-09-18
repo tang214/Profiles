090fde7ce9354b123ead1a2f3b65570fde01d9c5
Problem <pboyd04@gmail.com>
2014-09-18 22:55:42 +0000

    Add support for common javascript

--- class.ProfilesPage.php
+++ class.ProfilesPage.php
@@ -54,8 +54,8 @@ class ProfilesPage extends FlipPage
         }
         $about_menu = array(
             'Burning Flipside'=>'http://www.burningflipside.com/about/event',
-            'AAR, LLC'=>'http://www.burningflipside.com/about/aar',
-            'Privacy Policy'=>'http://www.burningflipside.com/about/privacy'
+            'AAR, LLC'=>'http://www.burningflipside.com/LLC',
+            'Privacy Policy'=>'http://www.burningflipside.com/about/rivacy'
         );
         $this->add_link('About', 'http://www.burningflipside.com/about', $about_menu);
     }
