65a9fe0e0161e57de41d09ae8614562f0db792c7
Problem <pboyd04@gmail.com>
2015-07-13 19:55:03 +0000

    Move pending users to seperate file to simplify API maintenance

--- api/v1/index.php
+++ api/v1/index.php
@@ -9,6 +9,7 @@ if($_SERVER['REQUEST_URI'][0] == '/' && $_SERVER['REQUEST_URI'][1] == '/')
 
 require('login.php');
 require('users.php');
+require('pending_users.php');
 
 $app = new FlipREST();
 $app->post('/login', 'login');
@@ -23,79 +24,6 @@ $app->patch('/areas/:name', 'update_area');
 $app->post('/areas', 'create_area');
 $app->get('/sessions', 'get_sessions');
 $app->delete('/sessions/:id', 'end_session');
-
-function list_pending_users()
-{
-    global $app;
-    if(!$app->user)
-    {
-        throw new Exception('Must be logged in', ACCESS_DENIED);
-    }
-    if(!$app->user->isInGroupNamed("LDAPAdmins"))
-    {
-        throw new Exception('Must be Admin', ACCESS_DENIED);
-    }
-    $auth = AuthProvider::getInstance();
-    $users = $auth->get_pending_users_by_filter(false, $app->odata->filter, $app->odata->select, $app->odata->top, $app->odata->skip, $app->odata->orderby);
-    echo json_encode($users);
-}
-
-function show_pending_user($hash)
-{
-    global $app;
-    if(!$app->user)
-    {
-        throw new Exception('Must be logged in', ACCESS_DENIED);
-    }
-    if(!$app->user->isInGroupNamed("LDAPAdmins"))
-    {
-        throw new Exception('Must be Admin', ACCESS_DENIED);
-    }
-    else
-    {
-        $user = \AuthProvider::getInstance()->get_pending_users_by_filter(false, new \Data\Filter("hash eq $hash"));
-    }
-    if($user === false) $app->halt(404);
-    if(!is_object($user) && isset($user[0]))
-    {
-        $user = $user[0];
-    }
-    echo $user->serializeObject();
-}
-
-function delete_pending_user($hash)
-{
-    global $app;
-    if(!$app->user)
-    {
-        throw new Exception('Must be logged in', ACCESS_DENIED);
-    }
-    if(!$app->user->isInGroupNamed("LDAPAdmins"))
-    {
-        throw new Exception('Must be Admin', ACCESS_DENIED);
-    }
-    else
-    {
-        $res = \AuthProvider::getInstance()->delete_pending_users_by_filter(false, new \Data\Filter("hash eq $hash"));
-        echo json_encode($res);
-    }
-}
-
-function activate_user($hash)
-{
-    global $app;
-    if(!$app->user)
-    {
-        throw new Exception('Must be logged in', ACCESS_DENIED);
-    }
-    else
-    {
-        $auth = \AuthProvider::getInstance();
-        $user = $auth->get_pending_users_by_filter(false, new \Data\Filter("hash eq $hash"));
-        $res = $auth->active_user(false, $user);
-        echo json_encode($res);
-    }
-}
 
 function list_groups()
 {
@@ -295,15 +223,6 @@ function end_session($id)
     }
     $ret = FlipSession::delete_session_by_id($id);
     echo json_encode($ret);
-}
-
-function pending_users()
-{
-    global $app;
-    $app->get('', 'list_pending_users');
-    $app->get('/:uid', 'show_pending_user');
-    $app->delete('/:uid', 'delete_pending_user');
-    $app->post('/:uid/Actions/activate', 'activate_user');
 }
 
 function groups()
