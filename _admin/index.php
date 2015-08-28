a3be58b3f315c5923fe10a82d3659d34794cb27a
Problem <pboyd04@gmail.com>
2015-08-28 19:27:07 +0000

    Update to new abstracted card interface
    Conflicts:
    	_admin/index.php

--- _admin/index.php
+++ _admin/index.php
@@ -25,96 +25,14 @@ $page->body .= '
         <h1 class="page-header">Dashboard</h1>
     </div>       
 </div>
-<div class="row">
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <span class="glyphicon glyphicon-user" style="font-size: 5em;"></span>                                
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div style="font-size: 40px;">'.$user_count.'</div>
-                        <div>Users</div>
-                    </div>
-                </div>
-            </div>
-            <a href="users_current.php">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right glyphicon glyphicon-circle-arrow-right"></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-green">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <span class="glyphicon glyphicon-inbox" style="font-size: 5em;"></span>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div style="font-size: 40px;">'.$temp_user_count.'</div>
-                        <div>Pending Users</div>
-                    </div>
-                </div>
-            </div>
-            <a href="users_pending.php">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right glyphicon glyphicon-circle-arrow-right"></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-red">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <span class="glyphicon glyphicon-tower" style="font-size: 5em;"></span>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div style="font-size: 40px;">'.$group_count.'</div>
-                        <div>Groups</div>
-                    </div>
-                </div>
-            </div>
-            <a href="groups.php">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right glyphicon glyphicon-circle-arrow-right"></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-yellow">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <span class="glyphicon glyphicon-cloud" style="font-size: 5em;"></span>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div style="font-size: 40px;">'.$session_count.'</div>
-                        <div>Sessions</div>
-                    </div>
-                </div>
-            </div>
-            <a href="sessions.php">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right glyphicon glyphicon-circle-arrow-right"></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-</div>';
+<div class="row">';
+
+$page->add_card('fa-user', $user_count, 'Users', 'users_current.php');
+$page->add_card('fa-inbox', $temp_user_count, 'Pending Users', 'users_pending.php', $page::CARD_GREEN);
+$page->add_card('fa-users', $group_count, 'Groups', 'groups.php', $page::CARD_RED);
+$page->add_card('fa-cloud', $session_count, 'Sessions', 'sessions.php', $page::CARD_YELLOW);
+
+$page->body .= '</div>';
 
 $page->print_page();
 ?>
