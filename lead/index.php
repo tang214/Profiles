171471a2a56a5abe76a7e5de913d7f545dca070f
Problem <pboyd04@gmail.com>
2015-08-28 19:28:18 +0000

    Use new card abstraction interface

--- lead/index.php
+++ lead/index.php
@@ -21,96 +21,14 @@ $page->body .= '
         <h1 class="page-header">Dashboard</h1>
     </div>       
 </div>
-<div class="row">
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <span class="glyphicon glyphicon-user" style="font-size: 5em;"></span>                                
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div style="font-size: 40px;">'.$lead_count.'</div>
-                        <div>Leads</div>
-                    </div>
-                </div>
-            </div>
-            <a href="directory.php?filter=lead">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right glyphicon glyphicon-circle-arrow-right"></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-green">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <span class="glyphicon glyphicon-bullhorn" style="font-size: 5em;"></span>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div style="font-size: 40px;">'.$af_count.'</div>
-                        <div>AFs</div>
-                    </div>
-                </div>
-            </div>
-            <a href="directory.php?filter=af">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right glyphicon glyphicon-circle-arrow-right"></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-yellow">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <span class="glyphicon glyphicon-subtitles" style="font-size: 5em;"></span>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div style="font-size: 40px;">'.$cc_count.'</div>
-                        <div>Combustion Chamber</div>
-                    </div>
-                </div>
-            </div>
-            <a href="directory.php?filter=cc">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right glyphicon glyphicon-circle-arrow-right"></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-6">
-        <div class="panel panel-red">
-            <div class="panel-heading">
-                <div class="row">
-                    <div class="col-xs-3">
-                        <span class="glyphicon glyphicon-fire" style="font-size: 5em;"></span>
-                    </div>
-                    <div class="col-xs-9 text-right">
-                        <div style="font-size: 40px;">'.$aar_count.'</div>
-                        <div>Board Members</div>
-                    </div>
-                </div>
-            </div>
-            <a href="directory.php?filter=aar">
-                <div class="panel-footer">
-                    <span class="pull-left">View Details</span>
-                    <span class="pull-right glyphicon glyphicon-circle-arrow-right"></span>
-                    <div class="clearfix"></div>
-                </div>
-            </a>
-        </div>
-    </div>
-</div>';
+<div class="row">';
+
+$page->add_card('fa-user', $lead_count, 'Leads', 'directory.php?filter=lead');
+$page->add_card('fa-bullhorn', $af_count, 'AFs', 'directory.php?filter=af', $page::CARD_GREEN);
+$page->add_card('fa-cc', $cc_count, 'Combustion Chamger', 'directory.php?filter=cc', $page::CARD_YELLOW);
+$page->add_card('fa-fire', $aar_count, 'Board Members', 'directory.php?filter=aar', $page::CARD_RED);
+
+$page->body .='</div>';
 
 $page->print_page();
 ?>
