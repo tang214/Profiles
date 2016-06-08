19490ae61b2eb31d73643a17a4130c09fc25da3a
Patrick Boyd <Patrick_Boyd@Dell.com>
2016-06-08 23:30:14 +0000

    Reduce complexity of getGroup

--- api/v1/groups.php
+++ api/v1/groups.php
@@ -29,20 +29,95 @@ function listGroups()
     }
 }
 
+function hasUser($app)
+{
+    return ($app->user || $app->isLocal);
+}
+
+function isAdmin($app)
+{
+    return ($app->isLocal || $app->user->isInGroupNamed('LDAPAdmins'));
+}
+
+function expandGroupMembers($group, $odata, $directOnly)
+{
+    if($odata->expand !== false && in_array('member', $odata->expand))
+    {
+        $ret = array();
+        $ret['cn'] = $group->getGroupName();
+        $ret['description'] = $group->getDescription();
+        $ret['member'] = $group->members(true, ($directOnly !== true));
+        return json_decode(json_encode($ret), true);
+    }
+    else if($directOnly)
+    {
+        $ret = array();
+        $ret['cn'] = $group->getGroupName();
+        $ret['description'] = $group->getDescription();
+        $ret['member'] = $group->getMemberUids(false);
+        return json_decode(json_encode($ret), true);
+    }
+    return json_decode(json_encode($group), true);
+}
+
+function getFlippedKeys($keys)
+{
+    $ret = array();
+    $count = count($keys);
+    for($i = 0; $i < $count; $i++)
+    {
+        $key = $keys[$i];
+        if(strstr($key, '.'))
+        {
+            $parts = explode('.', $key);
+            $tmp = array_shift($parts);
+            if(!isset($flipped[$tmp]))
+            {
+                $ret[$tmp] = array();
+            }
+            $ret[$tmp][] = $parts[0];
+            continue;
+        }
+        $ret[$key] = 1;
+    }
+    return $ret;
+}
+
+function selectFieldsFromGroup($group, $select)
+{
+    if($select !== false)
+    {
+        $flipped = getFlippedKeys($select);
+        foreach($flipped as $key=>$value)
+        {
+            if($value !== 1)
+            {
+                $tmp = array_flip($value);
+                if(isset($group[$key][0]))
+                {
+                    $count = count($group[$key]);
+                    for($i = 0; $i < $count; $i++)
+                    {
+                        $group[$key][$i] = array_intersect_key($group[$key][$i], $tmp);
+                    }
+                    continue;
+                }
+                $group[$key] = array_intersect_key($group[$key], $tmp);
+            }
+        }
+    }
+    return $group;
+}
+
 function getGroup($name)
 {
     global $app;
-    $isLocal = false;
-    if($_SERVER['SERVER_ADDR'] === $_SERVER['REMOTE_ADDR'])
-    {
-        $isLocal = true;
-    }
-    if(!$app->user && !$isLocal)
+    if(!hasUser($app))
     {
         $app->response->setStatus(401);
         return;
     }
-    if(($isLocal === true) || $app->user->isInGroupNamed('LDAPAdmins'))
+    if(isAdmin($app))
     {
         $auth = AuthProvider::getInstance();
         $users = $auth->getGroupByName($name);
@@ -52,78 +127,8 @@ function getGroup($name)
         {
             $directOnly = true;
         }
-        if($app->odata->expand !== false)
-        {
-            if(in_array('member', $app->odata->expand))
-            {
-                $group = array();
-                $group['cn'] = $users->getGroupName();
-                $group['description'] = $users->getDescription();
-                if($directOnly)
-                {
-                    $group['member'] = $users->members(true, false);
-                }
-                else
-                {
-                    $group['member'] = $users->members(true);
-                }
-                $users = json_decode(json_encode($group), true);
-            }
-        }
-        else if($directOnly)
-        {
-            $group = array();
-            $group['cn'] = $users->getGroupName();
-            $group['description'] = $users->getDescription();
-            $group['member'] = $users->getMemberUids(false);
-            $users = json_decode(json_encode($group), true);
-        }
-        else
-        {
-            $users = json_decode(json_encode($users), true);
-        }
-        if($app->odata->select !== false)
-        {
-            $keys = $app->odata->select;
-            $flipped = array();
-            foreach($keys as $key)
-            {
-                if(strstr($key, '.'))
-                {
-                    $parts = explode('.', $key);
-                    $tmp = array_shift($parts);
-                    if(!isset($flipped[$tmp]))
-                    {
-                        $flipped[$tmp] = array();
-                    }
-                    array_push($flipped[$tmp], $parts[0]);
-                }
-                else
-                {
-                    $flipped[$key] = 1;
-                }
-            }
-            foreach($flipped as $key=>$value)
-            {
-                if($value !== 1)
-                {
-                    $tmp = array_flip($value);
-                    if(isset($users[$key][0]))
-                    {
-                        $count = count($users[$key]);
-                        for($i = 0; $i < $count; $i++)
-                        {
-                            $users[$key][$i] = array_intersect_key($users[$key][$i], $tmp);
-                        }
-                    }
-                    else
-                    {
-                        $users[$key] = array_intersect_key($users[$key], $tmp);
-                    }
-                }
-            }
-            $users = array_intersect_key($users, $flipped);
-        }
+        $users = expandGroupMembers($users, $app->odata, $directOnly);
+        $users = selectFieldsFromGroup($users, $app->odata->select);
         echo json_encode($users);
     }
     else
@@ -240,5 +245,4 @@ function getNonGroupMembers($name)
     }
     echo json_encode($res);
 }
-
-?>
+/* vim: set tabstop=4 shiftwidth=4 expandtab: */
