dfdfe6c6f0fd1b9a0fb1cb5e74fa7e45bc28ecca
Russell <russell.t.sherman@gmail.com>
2017-02-23 15:55:16 -0600

    soften links
    # Conflicts:
    #	api/v1/.htaccess

--- api/v1/class.PasswordResetEmail.php
+++ api/v1/class.PasswordResetEmail.php
@@ -18,7 +18,7 @@ class PasswordResetEmail extends FlipsideProfileEmail
     {
         return 'Someone (quite possibly you) has requested a password reset of your Flipside account.<br/>
                 To reset your password click on the link below.<br/>
-                <a href="https://profiles.burningflipside.com/change.php?hash='.$this->user->getPasswordResetHash().'">Reset Password</a><br/>
+                <a href="'.$this->profilesUrl.'/change.php?hash='.$this->user->getPasswordResetHash().'">Reset Password</a><br/>
                 If you did not request this reset, don\'t worry. This email was sent only to you and your password has not been changed.<br/>
                 If you receive many of these requests, you can notify the technology team (technology@burningflipside.com).<br/>
                 Thank you,<br/>
@@ -29,7 +29,7 @@ class PasswordResetEmail extends FlipsideProfileEmail
     {
         return 'Someone (quite possibly you) has requested a password reset of your Flipside account.
                 To reset your password copy the following URL into your browser.
-                https://profiles.burningflipside.com/change.php?hash='.$this->user->getPasswordResetHash().'
+                '.$this->profilesUrl.'/change.php?hash='.$this->user->getPasswordResetHash().'
                 If you did not request this reset, don\'t worry. This email was sent only to you and your password has not been changed.
                 If you receive many of these requests, you can notify the technology team (technology@burningflipside.com).
                 Thank you,
