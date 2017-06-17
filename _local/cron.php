a210695c05c48f894e39a885f900dbeee51538ef
Problem <pboyd04@gmail.com>
2017-06-17 17:20:56 +0000

    Delete pending users that are older than 24 hour

--- _local/cron.php
+++ _local/cron.php
@@ -1,3 +1,16 @@
 <?php
+require_once('Autoload.php');
 
-?>
+$now = new DateTime();
+$now->modify('-1 day');
+$now->setTime(0, 0);
+$yesterday = date("Y-m-d H:i:s", $now->getTimestamp());
+
+$auth = \AuthProvider::getInstance();
+$filter = new \Data\Filter("time lt '$yesterday'");
+$users = $auth->getPendingUsersByFilter($filter);
+$count = count($users);
+for($i = 0; $i < $count; $i++)
+{
+    $users[$i]->delete();
+}
